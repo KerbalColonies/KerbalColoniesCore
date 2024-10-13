826ea896c0e9bd918c409848e55cbb260f4f6388
AMPW-german <theampw@gmail.com>
2024-10-13 11:36:41 +0200

    Add project files.

---
+++ Configuration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace StaticColonization
+{
+
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    class SettingsLoader : MonoBehaviour
+    {
+        public void Awake()
+        {
+            // load settings when game start
+
+        }
+    }
+
+    /// <summary>
+    /// Reads and holds configuration parameters
+    /// </summary>
+    static class Configuration
+    {
+        public static bool enableLogging = true; //Enable this only in debug purposes as it floods the logs very much
+
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,40 @@
+﻿using Smooth.Compare;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace StaticColonization
+{
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class StaticColonization : MonoBehaviour
+    {
+        const string APP_NAME = "StaticColonization";
+
+        protected void Start()
+        {
+            writeDebug("Starting SC");
+        }
+
+
+        public void Update()
+        {
+            writeDebug("Update!!!");
+        }
+
+        void writeDebug(string text)
+        {
+            if (Configuration.enableLogging)
+            {
+                writeLog(text);
+            }
+        }
+
+        void writeLog(string text)
+        {
+            KSPLog.print(APP_NAME + ": " + text);
+        }
+    }
+}

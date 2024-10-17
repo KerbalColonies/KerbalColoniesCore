ddfb273f8fd7bcd63eba9a4696b8cafa947bc89a
AMPW-german <theampw@gmail.com>
2024-10-17 23:09:49 +0200

    testing a few things, added the GNU GPLv3 license

--- Main.cs
+++ Main.cs
@@ -6,6 +6,30 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using KerbalKonstructs;
+using LibNoise.Models;
+using Smooth.Algebraics;
+using static FinePrint.ContractDefs;
+using Expansions.Missions.Tests;
+using KerbalKonstructs.Core;
+using LibNoise;
+using static EdyCommonTools.RotationController;
+
+// KKK: Kerbal Konstructs Kolonization
+// This mod aimes to create a colony system with Kerbal Konstructs statics
+//Copyright (C) 2024 AMPW, lolsmcfee
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <https://www.gnu.org/licenses/
 
 namespace KerbalKonstructsKolonization
 {
@@ -19,12 +43,22 @@ namespace KerbalKonstructsKolonization
             writeDebug("Starting KKK");
         }
 
-
-        public void Update()
+        /// <summary>
+        /// </summary>
+        public void FixedUpdate()
         {
             if (Input.GetKeyDown(KeyCode.U))
             {
-                KerbalKonstructs.API.PlaceStatic("KK_1700m_C_runway", FlightGlobals.currentMainBody.name, FlightGlobals.ship_latitude, FlightGlobals.ship_longitude, (float) FlightGlobals.ship_altitude - 10f, 0f);
+                Vessel vessel = FlightGlobals.ActiveVessel;
+                writeDebug("Surface:" + vessel.GetHeightFromSurface().ToString());
+                writeDebug("Terrain:" + vessel.GetHeightFromTerrain().ToString()); 
+                if (vessel.srfSpeed <= 0.5f && vessel.Landed)
+                {
+
+                    string uuid = KerbalKonstructs.API.PlaceStatic("LandingZoneSmall", FlightGlobals.currentMainBody.name, FlightGlobals.ship_latitude, FlightGlobals.ship_longitude, vessel.GetHeightFromSurface() - 2, 0f);
+                    vessel.SetPosition(new Vector3d(vessel.latitude, vessel.longitude, vessel.GetHeightFromTerrain() + 2));
+                    KerbalKonstructs.API.HighLightStatic(uuid, Color.yellow);
+                }
             }
         }
 
